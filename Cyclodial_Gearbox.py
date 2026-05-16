87e1cf962857a55eaf4586da270fe349c22932d6
Alan Bonilla Santos <alan2006@bu.edu>
2026-05-16 16:39:26 -0500

    Update Cyclodial_Gearbox.py

--- Cyclodial_Gearbox.py
+++ Cyclodial_Gearbox.py
@@ -14,7 +14,7 @@ print(f"Your ratio is {Ratio}:1 reduction ratio for this gearbox.")
 Dr = 13       # diamater of the 686-2RS bearings (mm)
 Rr = 13/2     # Radius of bearings (mm)
 R = 90/2      # Radius of the bearings (mm)
-E = 1.5       # Eccentricity (mm)
+E = 1.5       # Eccentricity: Axis offset that allows high reduction ratios (mm)
 
 # Constraint for when making the cyclodial drive disks
 if E < Rr:
@@ -22,13 +22,18 @@ if E < Rr:
   
 # --- Cyclodial Gearbox Rotor Shape ---
 
-# Constraint: 
-theta = 
+# Defining rotors shape: 
+theta_degrees = 45
+theta = math.radians(theta_degrees)
 
-if 0 <= theta <= 360:
+if 0 <= theta_degrees <= 360:
   print(f"{theta} is within the constraint")
 
-psi = -math.atan(math.sin((1-N)theta)/((R/E*N) - math.cos(1-N)*theta)
+psi = -math.atan2(math.sin((1-N)*theta), ((R/(E*N)) - math.cos((1-N)*theta)))
 
+
+# Final Equation
 X = R*math.cos(theta) - Rr*math.cos(theta - psi) - E*math.cos(N*theta)
 Y = -R*math.sin(theta) + Rr*math.sin(theta - psi) + E*math.sin(N*theta)
+
+print(f"{X}, {Y}")
