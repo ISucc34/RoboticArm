28704402ae809beb3589aa00cf39d869a2797e0a
Alan Bonilla Santos <alan2006@bu.edu>
2026-05-16 13:35:32 -0500

    Update Cyclodial_Gearbox.py

--- Cyclodial_Gearbox.py
+++ Cyclodial_Gearbox.py
@@ -3,6 +3,23 @@
 # --- Libraries ---
 import math
 
+# --- Determining the parameters ---
+
+# The gearbox ratio
+N = 16                # Number of rollers
+Ratio = N - 1         # Ratio Number
+print(f"Your ratio is {Ratio}:1 reduction ratio for this gearbox.")
+
+# Constraints from parts
+Dr = 13       # diamater of the 686-2RS bearings (mm)
+Rr = 13/2     # Radius of bearings (mm)
+R = 90/2      # Radius of the bearings (mm)
+E = 1.5       # Eccentricity (mm)
+
+# Constraint for when making the cyclodial drive disks
+if E < Rr:
+  print(f"{E} is within the constraint.") 
+  
 # --- Cyclodial Gearbox Rotor Shape ---
 
 # Constraint: 
