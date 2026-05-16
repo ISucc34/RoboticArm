e489f9137480aaa4399315444a2db4e9432a454b
Alan Bonilla Santos <alan2006@bu.edu>
2026-05-16 11:26:33 -0500

    Update Cyclodial_Gearbox.py

--- Cyclodial_Gearbox.py
+++ Cyclodial_Gearbox.py
@@ -1,1 +1,17 @@
 # Written by Alan Bonilla Santos
+
+# --- Libraries ---
+import math
+
+# --- Cyclodial Gearbox Rotor Shape ---
+
+# Constraint: 
+theta = 
+
+if 0 <= theta <= 360:
+  print(f"{theta} is within the constraint")
+
+psi = -math.atan(math.sin((1-N)theta)/((R/E*N) - math.cos(1-N)*theta)
+
+X = R*math.cos(theta) - Rr*math.cos(theta - psi) - E*math.cos(N*theta)
+Y = -R*math.sin(theta) + Rr*math.sin(theta - psi) + E*math.sin(N*theta)
