5359b3ce9b7a5d9af4503bb6ba248dbbe58a3ddb
Isaac Kristopher Gonzalez <142045835+ISucc34@users.noreply.github.com>
2026-05-25 22:08:57 -0500

    Set up the code to change colors during runtime, needs bug fixes

--- Code/vision.py
+++ Code/vision.py
@@ -1,5 +1,5 @@
 import cv2 as cv
-
+import numpy as np
 
 
 
@@ -10,36 +10,62 @@ def main():
     cv.namedWindow("trackbars")
 
 
+    h_Max = 255
+    h_Min = 0
+
+    s_Max = 100
+    s_Min = 0
+
+    v_Max = 50
+    v_Min = 0
+
     def hueUpper(val):
-        print(val)
+        global h_Max
+        h_Max = val
+        print(h_Max)
 
     def hueLower(val):
-        print(val)
+        global h_Min
+        h_Min = val
 
     def satUpper(val):
-        print(val)
+        global s_Max
+        s_Max = val
 
     def satLower(val):
-        print(val)
+        global s_Min
+        s_Min = val
 
     def valUpper(val):
-        print(val)
+        global v_Max
+        v_Max = val
 
     def valLower(val):
-        print(val)
+        global v_Min
+        v_Min = val
     
     cv.createTrackbar("Hue Upper", "trackbars", 0, 255, hueUpper)  
     cv.createTrackbar("Hue Lower", "trackbars", 0, 255, hueLower)
     cv.createTrackbar("Saturation Upper", "trackbars", 0, 255, satUpper)
     cv.createTrackbar("Saturation Lower", "trackbars", 0, 255, satLower)
     cv.createTrackbar("Value Upper", "trackbars", 0, 255, valUpper)
-    cv.createTrackbar("Value Upper", "trackbars", 0, 255, valLower)
+    cv.createTrackbar("Value Lower", "trackbars", 0, 255, valLower)
     
-    while cam.isOpened():
+    while cam:
         ret, frame = cam.read()
 
+        lower_bound = np.array([h_Min, s_Min, v_Min])
+        upper_bound = np.array([h_Max, s_Max, v_Max])
+
+        HSVframe = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
+        mask = cv.inRange(HSVframe, lower_bound, upper_bound)
+
+        new = cv.bitwise_and(frame, frame, mask=mask)
+
         cv.imshow("Camera",frame)
-        #cv.imshow("trackbars")
+        cv.imshow("Mask", new)
+
 
         if cv.waitKey(1) == ord('q'):
             break
--- README.md
+++ README.md
@@ -8,14 +8,18 @@
 
 ### Through Conda
 
+Create a name of your choice for <envName>
+
 ``
 conda create -n <envName> python==3.13.5
 ``
+
 
 ``
 conda activate <envName>
 ``
 
+Install all dependencies
 ``
 pip install -r requirements.txt
 ``
@@ -25,5 +29,8 @@ pip install -r requirements.txt
 
 
 ## Technologies
+
+
+
 
 ## Methodology
