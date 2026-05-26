2e29aa2e8ca972b28df53cabe17840f06a4fb2a2
Isaac Kristopher Gonzalez <142045835+ISucc34@users.noreply.github.com>
2026-05-25 20:58:42 -0500

    Created trackbars

--- Code/vision.py
+++ Code/vision.py
@@ -2,18 +2,50 @@ import cv2 as cv
 
 
 
+
 def main():
 
     cam = cv.VideoCapture(0)
 
+    cv.namedWindow("trackbars")
+
+
+    def hueUpper(val):
+        print(val)
+
+    def hueLower(val):
+        print(val)
+
+    def satUpper(val):
+        print(val)
+
+    def satLower(val):
+        print(val)
+
+    def valUpper(val):
+        print(val)
+
+    def valLower(val):
+        print(val)
+    
+    cv.createTrackbar("Hue Upper", "trackbars", 0, 255, hueUpper)  
+    cv.createTrackbar("Hue Lower", "trackbars", 0, 255, hueLower)
+    cv.createTrackbar("Saturation Upper", "trackbars", 0, 255, satUpper)
+    cv.createTrackbar("Saturation Lower", "trackbars", 0, 255, satLower)
+    cv.createTrackbar("Value Upper", "trackbars", 0, 255, valUpper)
+    cv.createTrackbar("Value Upper", "trackbars", 0, 255, valLower)
     
     while cam.isOpened():
         ret, frame = cam.read()
 
         cv.imshow("Camera",frame)
+        #cv.imshow("trackbars")
 
         if cv.waitKey(1) == ord('q'):
             break
+    
+    cv.destroyAllWindows()
+
 
         
 
